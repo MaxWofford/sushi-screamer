366672e505d4f5c688719f0f9fc5c1aa03ae6755
Max Wofford <max@maxwofford.com>
2017-07-01 18:50:20 -0700

    Add stefans face.

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, Alert, TouchableHighlight } from 'react-native';
+
+class Rolls extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      amountRolls: props.amountRolls 
+    };
+  }
+
+  render() {
+    return  (
+      <Text>
+        Rolls: { this.state.amountRolls  }
+      </Text>
+    );
+  }
+}
+
 
 export default class App extends React.Component {
+  constructor() {
+    super(props);
+    this.state = {
+      amountRolls: 0;
+    };
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TouchableHighlight onPress = {() => Alert.alert('ohhh shit whatdup') }>
+          <Text>BUY BUY BUY</Text>
+        </TouchableHighlight>
+        
+        <TouchableHighlight onPress = {() => Alert.alert('ohhh shit whatdup') }>
+          <Image source={require('./assets/stefan.jpg')} />
+        </TouchableHighlight>
+
+        <Rolls amountRolls= {this.state.amountRolls}/>
+
       </View>
     );
   }
